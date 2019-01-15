77ee679a6aa629c2f8728b0849470994610b377d
NiraParikh <Angellegna023@gmail.com>
2019-01-15 18:23:02 -0500

    Done

---
+++ src/main/java/com/zipcodewilmington/streams/StreamMap.java
@@ -0,0 +1,42 @@
+package com.zipcodewilmington.streams;
+
+import com.zipcodewilmington.streams.anthropoid.Person;
+import com.zipcodewilmington.streams.conversions.StreamConverter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by leon on 5/24/17.
+ */
+public class StreamMap {
+    /**
+     * Section 8.3
+     * @param someWord - word to convert to Stream<String>
+     * @return - a Stream of single characters
+     */ //TODO
+        public static Stream<String> letters (String someWord) {
+
+            return Stream.of(someWord);
+    }
+
+    /**
+     * @param someWords - variable amount of String arguments
+     * @return - a Stream of several Streams of single characters
+     */ //TODO
+    public static Stream<Stream<String>> wordsMap(String... someWords) {
+        return Stream.of(someWords).map(x-> letters (x));
+
+    }
+
+    /**
+     * @param stringArray - variable amount of String arguments
+     * @return - a Stream of several Streams of single characters
+     */ //TODO
+    public static Stream<String> wordsFlatMap(String... stringArray) {
+        return Stream.of(stringArray).flatMap(x -> letters(x));
+    }
+}
---
+++ src/main/java/com/zipcodewilmington/streams/conversions/StreamConverter.java
@@ -0,0 +1,43 @@
+package com.zipcodewilmington.streams.conversions;
+
+import com.zipcodewilmington.streams.anthropoid.Person;
+import com.zipcodewilmington.streams.anthropoid.PersonFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by leon on 5/25/17.
+ */
+public final class StreamConverter extends PersonConversionAgent<Stream<Person>> {
+    private final List<Person> personList;
+
+    public StreamConverter(Stream<Person> people) {
+        super(people);
+        this.personList = super.objectSequence.collect(Collectors.toList());
+    }
+
+    public StreamConverter(int collectionSize) {
+        this(PersonFactory.createPersonStream(collectionSize));
+        /** Implementation of adapter pattern for testing purposes
+         *  @param collectionSize  - count of Stream to be generated */
+    }
+
+    // TODO
+    public List<Person> toList() {
+        return personList;
+    }
+
+    // TODO
+    public Stream<Person> toStream() {
+        return personList.stream();
+
+    }
+
+    // TODO
+    public Person[] toArray() {
+        return personList.stream().toArray(Person[]::new);
+
+    }
+}
